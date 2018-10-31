5ba04fda3381b7d816809c656fe27e1a859bc9d4
Oliver Zander <zander@cosmocode.de>
2018-10-31 15:00:53 +0100

    make tempdir yield a path object when available

--- utils/tempdir.py
+++ utils/tempdir.py
@@ -3,11 +3,16 @@ import tempfile
 
 from contextlib import contextmanager
 
+try:
+    from pathlib import Path
+except ImportError:
+    Path = str
+
 
 @contextmanager
 def maketempdir(onerror=None, **kwargs):
     directory = tempfile.mkdtemp(**kwargs)
 
-    yield directory
+    yield Path(directory)
 
     shutil.rmtree(directory, ignore_errors=onerror is None, onerror=onerror)
