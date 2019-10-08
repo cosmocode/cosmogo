30d4051490a213a1812f7808074365b8799b2271
Oliver Zander <zander@cosmocode.de>
2019-10-08 11:01:09 +0200

    added admin mixin

--- admin.py
+++ admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
+from django.utils.translation import ugettext_lazy as _
 
 
 class DefaultTabularInline(admin.TabularInline):
     template = 'admin/inline/tabular-without-original.html'
     extra = 0
+
+
+class AdminViewMixin:
+
+    def get_object_for_change(self: admin.ModelAdmin, request, object_id):
+        obj = self.get_object(request, object_id)
+
+        if not self.has_change_permission(request, obj=obj):
+            raise PermissionDenied
+
+        if obj is None:
+            msg = _('%(name)s object with primary key %(key)r does not exist.')
+            context = {'name': self.opts.verbose_name, 'key': object_id}
+
+            raise Http404(msg % context)
+
+        return obj
