e0eb6276cf287c8ae46853cdec72a913b0722b03
Oliver Zander <zander@cosmocode.de>
2020-03-24 17:56:56 +0100

    added get_field method to sql utils

--- utils/sql.py
+++ utils/sql.py
@@ -2,6 +2,7 @@ from typing import Type
 
 from django.db import DEFAULT_DB_ALIAS, connections
 from django.db.models import Model
+from django.db.models.options import Options
 
 
 def table(model: Type[Model], using: str = None) -> str:
@@ -15,6 +16,10 @@ def table(model: Type[Model], using: str = None) -> str:
     return quote(db_table, using)
 
 
+def get_field(opts: Options, field: str):
+    return opts.pk if field == 'pk' else opts.get_field(field)
+
+
 def column(model: Type[Model], field: str, using: str = None) -> str:
     """
     Returns an absolute quoted column name suitable to be used in a raw query.
@@ -22,7 +27,7 @@ def column(model: Type[Model], field: str, using: str = None) -> str:
 
     opts = model._meta
     db_table = opts.db_table
-    db_field = opts.pk if field == 'pk' else opts.get_field(field)
+    db_field = get_field(opts, field)
     db_column = db_field.column
 
     return '{table}.{column}'.format(
