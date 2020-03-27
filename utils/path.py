c3958b862a9386847e3c084e59df5e552ef8df42
Oliver Zander <zander@cosmocode.de>
2020-03-27 17:58:47 +0100

    added ensure directory helper

--- utils/path.py
+++ utils/path.py
@@ -2,11 +2,21 @@ import os
 
 from contextlib import contextmanager
 
+from .filepath import FilePath
+
 
 @contextmanager
-def working_directory(path):
+def working_directory(path: FilePath):
     current = os.getcwd()
 
     yield os.chdir(path)
 
     os.chdir(current)
+
+
+def ensure_directory(filepath: FilePath):
+    directory = os.path.dirname(filepath)
+
+    os.makedirs(directory, exist_ok=True)
+
+    return directory
