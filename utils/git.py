cdd9190b727d59070a4ec19dc4c72a1e7300a04c
Karsten Kosmala <kosmalakarsten@gmail.com>
2021-06-09 12:48:26 +0200

    during ci job use environment to get commit hash

--- utils/git.py
+++ utils/git.py
@@ -3,6 +3,9 @@ import subprocess
 
 
 def get_commit(path, revision='HEAD'):
+    if os.environ.get('CI') == 'true':
+        return os.environ.get('CI_COMMIT_SHA')
+
     path = os.path.abspath(path)
     command = ['git', 'rev-parse', revision]
 
