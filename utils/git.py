9edf96d900798347bf22b36eac62986fe4ef6538
Karsten Kosmala <kosmala@cosmocode.de>
2021-06-09 10:55:19 +0000

    use environment var if it is set

--- utils/git.py
+++ utils/git.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 def get_commit(path, revision='HEAD'):
-    if os.environ.get('CI') == 'true':
+    if os.environ.get('CI_COMMIT_SHA'):
         return os.environ.get('CI_COMMIT_SHA')
 
     path = os.path.abspath(path)
