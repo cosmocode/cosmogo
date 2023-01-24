03e03ba10ea783b45becf5b881ff8d9dda5e68f2
Oliver Zander <zander@cosmocode.de>
2023-01-24 10:57:13 +0100

    added util to create an aware datetime

--- utils/timezone.py
+++ utils/timezone.py
@@ -4,6 +4,12 @@ import datetime
 from django.utils import timezone
 
 
+def create(year, month=1, day=1, hour=0, minute=0, second=0, microsecond=0, tz=None):
+    value = datetime.datetime(year, month, day, hour, minute, second, microsecond)
+
+    return timezone.make_aware(value, timezone=tz)
+
+
 def timestamp(value: datetime.datetime) -> float:
     """
     Helper to convert a datetime to a unix timestamp.
