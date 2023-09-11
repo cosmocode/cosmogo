7fba8975eb31c7267a28efcc10ed95c325d2573d
Oliver Zander <zander@cosmocode.de>
2023-09-11 14:55:41 +0200

    improved admin utils

--- admin.py
+++ admin.py
@@ -5,8 +5,9 @@ from typing import Type
 from django.contrib import admin
 from django.core.exceptions import PermissionDenied
 from django.db.models import Model
-from django.http import Http404
-from django.urls import reverse
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse, path
 from django.utils.html import format_html
 
 from cosmogo.utils.gettext import trans
@@ -19,17 +20,48 @@ def admin_url(model: Type[Model], view: str, *args, site=None, **kwargs) -> str:
     Return an url to an admin view.
     """
 
-    opts = model._meta
-    site = site or admin.site
-    info = site.name, opts.app_label, opts.model_name, view
+    return reverse(admin_url_name(model, view, site=site), args=args, kwargs=kwargs)
 
-    return reverse('%s:%s_%s_%s' % info, args=args, kwargs=kwargs)
+
+def admin_redirect(model: Type[Model], view: str, *args, site=None, **kwargs) -> HttpResponseRedirect:
+    return redirect(admin_url_name(model, view, site=site), *args, **kwargs)
+
+
+def admin_url_name(model: Type[Model], view: str, *, site=None) -> str:
+    namespace = (site or admin.site).name
+    view_name = admin_view_name(model, view)
+
+    return f'{namespace}:{view_name}'
+
+
+def admin_view_name(model: Type[Model], view: str) -> str:
+    return f'{model._meta.app_label}_{model._meta.model_name}_{view}'
+
+
+def admin_path(model_admin: admin.ModelAdmin, route, view, name):
+    name = admin_view_name(model_admin.model, name)
+    view = model_admin.admin_site.admin_view(view)
+
+    return path(route, view, name=name)
 
 
 def admin_link(obj: Model, view='change', site=None, label=None, template=DEFAULT_LINK_TEMPLATE):
     url = admin_url(type(obj), view, obj.pk, site=site)
 
     return format_html(template, url=url, obj=obj, label=label or obj)
+
+
+def admin_template_names(model: Type[Model], name: str, *, site=None):
+    opts = model._meta
+    app_label = opts.app_label
+    model_name = opts.model_name
+    namespace = (site or admin.site).name
+
+    return [
+        f'{namespace}/{app_label}/{model_name}/{name}.html',
+        f'{namespace}/{app_label}/{name}.html',
+        f'{namespace}/{name}.html',
+    ]
 
 
 def json_display(data):
