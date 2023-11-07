5e5ee0dd8255a798e96b826642e0f70f2b48bba3
Oliver Zander <zander@cosmocode.de>
2023-11-07 16:15:56 +0100

    improved ip address util

--- utils/request.py
+++ utils/request.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 
 
-def get_ip_address(request, default='0.0.0.0'):
-    if getattr(settings, 'USE_X_FORWARDED_FOR', False):
+def get_ip_address(request, default='0.0.0.0', *, use_x_forwarded_for=False):
+    if getattr(settings, 'USE_X_FORWARDED_FOR', use_x_forwarded_for):
         forwarded = request.META.get('HTTP_X_FORWARDED_FOR')
 
         if forwarded:
             client, *proxys = forwarded.split(',', 1)
+            client = str.strip(client)
 
             if client:
                 return client
