f02fe8ff810b237cd02d9f781a683e60102b2614
Fabian Niemann <niemann@cosmocode.de>
2024-07-09 08:38:27 +0200

    Add git_commit_short helper

--- utils/git.py
+++ utils/git.py
@@ -15,3 +15,18 @@ def get_commit(path, revision='HEAD'):
     output = subprocess.check_output(command, cwd=path, encoding='utf-8')
 
     return str.strip(output)
+
+
+def get_commit_short(path, revision='HEAD'):
+    if ci_commit_short_sha := os.getenv('CI_COMMIT_SHORT_SHA'):
+        return ci_commit_short_sha
+
+    path = os.path.abspath(path)
+    command = ['git', 'rev-parse', '--short', revision]
+
+    if os.path.isfile(path):
+        path = os.path.dirname(path)
+
+    output = subprocess.check_output(command, cwd=path, encoding='utf-8')
+
+    return str.strip(output)
