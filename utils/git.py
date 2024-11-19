000ba18b4b522c41d6b30061ac6e72b84f5ceb44
Oliver Zander <zander@cosmocode.de>
2024-11-19 14:40:54 +0100

    deduplicate git tools

--- utils/git.py
+++ utils/git.py
@@ -2,27 +2,16 @@ import os
 import subprocess
 
 
-def get_commit(path, revision='HEAD'):
-    if ci_commit_sha := os.getenv('CI_COMMIT_SHA'):
+def get_commit(path, *, short=False, revision='HEAD', env_key='CI_COMMIT_SHA'):
+    if ci_commit_sha := os.getenv(env_key):
         return ci_commit_sha
 
-    path = os.path.abspath(path)
     command = ['git', 'rev-parse', revision]
 
-    if os.path.isfile(path):
-        path = os.path.dirname(path)
-
-    output = subprocess.check_output(command, cwd=path, encoding='utf-8')
-
-    return str.strip(output)
-
-
-def get_commit_short(path, revision='HEAD'):
-    if ci_commit_short_sha := os.getenv('CI_COMMIT_SHORT_SHA'):
-        return ci_commit_short_sha
+    if short:
+        command.insert(2, '--short')
 
     path = os.path.abspath(path)
-    command = ['git', 'rev-parse', '--short', revision]
 
     if os.path.isfile(path):
         path = os.path.dirname(path)
@@ -30,3 +19,7 @@ def get_commit_short(path, revision='HEAD'):
     output = subprocess.check_output(command, cwd=path, encoding='utf-8')
 
     return str.strip(output)
+
+
+def get_commit_short(path, *, revision='HEAD'):
+    return get_commit(path, short=True, revision=revision, env_key='CI_COMMIT_SHORT_SHA')
