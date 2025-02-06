629520909e0b32a6af4a8befcd8a966ec9ad7cf0
Oliver Zander <zander@cosmocode.de>
2025-02-06 11:42:55 +0100

    add more panels to disabled panels

--- utils/settings.py
+++ utils/settings.py
@@ -66,6 +66,7 @@ def debug_toolbar(apps, middleware, active=True, **config):
             # We disable the rarely used panels
             # by default to improve performance.
             'DISABLE_PANELS': {
+                'debug_toolbar.panels.history.HistoryPanel',
                 'debug_toolbar.panels.versions.VersionsPanel',
                 'debug_toolbar.panels.timer.TimerPanel',
                 'debug_toolbar.panels.settings.SettingsPanel',
@@ -73,10 +74,12 @@ def debug_toolbar(apps, middleware, active=True, **config):
                 'debug_toolbar.panels.request.RequestPanel',
                 'debug_toolbar.panels.staticfiles.StaticFilesPanel',
                 'debug_toolbar.panels.templates.TemplatesPanel',
+                'debug_toolbar.panels.alerts.AlertsPanel',
                 'debug_toolbar.panels.cache.CachePanel',
                 'debug_toolbar.panels.signals.SignalsPanel',
                 'debug_toolbar.panels.logging.LoggingPanel',
                 'debug_toolbar.panels.redirects.RedirectsPanel',
+                'debug_toolbar.panels.profiling.ProfilingPanel',
             },
             **config,
         }
